cce062c4e70363f27e0e0fe695accfe071306448
doomed-phobos <the_ekj.2121@outlook.com>
2026-06-26 00:03:13 -0500

    test: added test for dialog_parser

--- src/dialog_parser.rs
+++ src/dialog_parser.rs
@@ -1,6 +1,6 @@
 use chumsky::prelude::*;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Token<'a> {
   Block(Block<'a>),      // Either [MyConversation] or [[MyChoicer]]
   Speaker(&'a str),      // Saya:
@@ -10,13 +10,13 @@ pub enum Token<'a> {
   End                    // ===
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Block<'a> {
   Conversation(&'a str), // [MyConversation]
   Choicer(&'a str),      // [[MyChoicer]]
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Command<'a> {
   Wait(f32),               // @wait 2.3
   Jump(Block<'a>),         // @jump [Conversation], @jump [[Choicer]]
@@ -63,8 +63,11 @@ pub fn dialog_block_lexer<'a>() -> impl Parser<'a, &'a str, Vec<Token<'a>>> {
     .map(Token::Block);
 
   let speaker =
-    text::ident()
-      .padded()
+    any()
+      .filter(|c: &char| c.is_ascii_alphanumeric() || *c == '-')
+      .repeated()
+      .at_least(1)
+      .to_slice()
       .then_ignore(just(':'))
       .map(Token::Speaker);
 
@@ -96,7 +99,6 @@ pub fn dialog_block_lexer<'a>() -> impl Parser<'a, &'a str, Vec<Token<'a>>> {
         choice((
             block_choicer,
             block_conversation)))
-      .padded()
       .map(Command::Jump);
 
   let wait =
@@ -143,7 +145,7 @@ pub fn dialog_block_lexer<'a>() -> impl Parser<'a, &'a str, Vec<Token<'a>>> {
   // ==========================  
   
   let line = 
-    none_of('\n')
+    none_of("@[]=:\n")
       .repeated()
       .at_least(1)
       .to_slice()
@@ -170,4 +172,143 @@ fn parse_float<'a>() -> impl Parser<'a, &'a str, f32> {
     .then(frac.or_not())
     .to_slice()
     .map(|s: &str| s.parse().unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn parsing_number() {
+    let res = parse_float().parse("4").into_output();
+
+    assert_eq!(res, Some(4.0));
+  }
+
+  #[test]
+  fn parsing_zero_with_frac() {
+    let res = parse_float().parse("0.2").into_output();
+
+    assert_eq!(res, Some(0.2));
+  }
+
+  #[test]
+  fn parsing_zero_with_leading_zero_frac() {
+    let res = parse_float().parse("0.02").into_output();
+
+    assert_eq!(res, Some(0.02));
+  }
+
+  #[test]
+  fn parsing_block_conversation() {
+    let res = dialog_block_lexer().parse("[Conversation]").into_output();
+
+    assert_eq!(res, Some(vec![Token::Block(Block::Conversation("Conversation"))]));
+  }
+
+  #[test]
+  fn parsing_block_choicer() {
+    let res = dialog_block_lexer().parse("[[Choicer]]").into_output();
+
+    assert_eq!(res, Some(vec![Token::Block(Block::Choicer("Choicer"))]));
+  }
+
+  #[test]
+  fn parsing_speaker() {
+    let res = dialog_block_lexer().parse("Saya-Chan:").into_output();
+    let res1 = dialog_block_lexer().parse("Player:").into_output();
+    let res2 = dialog_block_lexer().parse("  Kaori   :").into_output();
+
+    assert_eq!(res, Some(vec![Token::Speaker("Saya-Chan")]));
+    assert_eq!(res1, Some(vec![Token::Speaker("Player")]));
+    assert_eq!(res2, None);
+  }
+
+  #[test]
+  fn parsing_choice() {
+    let res = dialog_block_lexer().parse("-> This is my choice!!!").into_output();
+
+    assert_eq!(res, Some(vec![Token::Choice("This is my choice!!!")]));
+  }
+
+  #[test]
+  fn parsing_command() {
+    let res = dialog_block_lexer().parse("@jump [Conversation]").into_output();
+    assert_eq!(res, Some(vec![Token::Command(Command::Jump(Block::Conversation("Conversation")))]));
+    let res = dialog_block_lexer().parse("@jump [[Choicer]]").into_output();
+    assert_eq!(res, Some(vec![Token::Command(Command::Jump(Block::Choicer("Choicer")))]));
+    let res = dialog_block_lexer().parse("@jump").into_output();
+    assert_eq!(res, None);
+    let res = dialog_block_lexer().parse("@wait 1.3").into_output();
+    assert_eq!(res, Some(vec![Token::Command(Command::Wait(1.3))]));
+    let res = dialog_block_lexer().parse("@wait").into_output();
+    assert_eq!(res, None);
+    let res = dialog_block_lexer().parse("@wait a").into_output();
+    assert_eq!(res, None);
+    let res = dialog_block_lexer().parse("@set Anim.Value").into_output();
+    assert_eq!(res, Some(vec![Token::Command(Command::Set{r#enum: "Anim", value: "Value"})]));
+    let res = dialog_block_lexer().parse("@set Anim").into_output();
+    assert_eq!(res, None);
+    let res = dialog_block_lexer().parse("@set").into_output();
+    assert_eq!(res, None);
+    let res = dialog_block_lexer().parse("@setmainchoicer [Hola]").into_output();
+    assert_eq!(res, None);
+    let res = dialog_block_lexer().parse("@setmainchoicer").into_output();
+    assert_eq!(res, None);
+    let res = dialog_block_lexer().parse("@setmainchoicer [[Hola]]").into_output();
+    assert_eq!(res, Some(vec![Token::Command(Command::SetMainChoicer("Hola"))]));
+    let res = dialog_block_lexer().parse("@next").into_output();
+    assert_eq!(res, Some(vec![Token::Command(Command::Next)]));
+  }
+
+  #[test]
+  fn full_script_parsing() {
+      let input = r#"
+  [Conversation01]
+  saya:
+    Hello there!
+  player:
+    -> First option
+    -> Second option
+  saya:
+    @wait 1.5
+    @jump [[Choicer01]]
+  player:
+    @set Game.state
+    @next
+  ===
+  "#;
+
+      let tokens = dialog_block_lexer()
+          .parse(input)
+          .into_result()
+          .unwrap();
+
+      assert_eq!(
+          tokens,
+          vec![
+              Token::Block(Block::Conversation("Conversation01")),
+              
+              Token::Speaker("saya"),
+              Token::Text("Hello there!"),
+              
+              Token::Speaker("player"),
+              Token::Choice("First option"),
+              Token::Choice("Second option"),
+
+              Token::Speaker("saya"),
+              Token::Command(Command::Wait(1.5)),
+              Token::Command(Command::Jump(Block::Choicer("Choicer01"))),
+
+              Token::Speaker("player"),
+              Token::Command(Command::Set {
+                  r#enum: "Game",
+                  value: "state",
+              }),
+
+              Token::Command(Command::Next),
+              Token::End,
+          ]
+      );
+  }
 }
